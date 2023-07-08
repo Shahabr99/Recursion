c255191d368308dc62ba7aed97059571b530f580
Shahabr99 <sean.rohani9@gmail.com>
2023-07-08 00:31:52 -0700

    assignment done

--- recursion.js
+++ recursion.js
@@ -67,15 +67,49 @@ function findIndex(arr, val, i=0) {
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
+function revString(str, i=str.length-1, string="") {
+  if(i < 0) return string
 
+  string += str[i]
+
+  return revString(str, i-1, string)
 }
+
+
 
 /** gatherStrings: given an object, return an array of all of the string values. */
+let nestedObj = {
+  firstName: "Lester",
+  favoriteNumber: 22,
+  moreData: {
+    lastName: "Testowitz"
+  },
+  funFacts: {
+    moreStuff: {
+      anotherNumber: 100,
+      deeplyNestedString: {
+        almostThere: {
+          success: "you made it!"
+        }
+      }
+    },
+    favoriteString: "nice!"
+  }
+};
 
-function gatherStrings(obj) {
 
+function gatherStrings(obj, result=[]) {
+  for(let key in obj) {
+    if(typeof obj[key] === "string") {
+      result.push(obj[key])
+    }else if(typeof obj[key] === "object") {
+      result = result.concat(gatherStrings(obj[key]));
+    }
+  }
+  return result
 }
+
+console.log(gatherStrings(nestedObj)) // ["Lester", "Testowitz", "you made it!", "nice!"];
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
